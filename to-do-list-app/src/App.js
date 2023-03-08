681c2d39ff3e622bbd7a7f4d97aeaf404a7664cc
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-03-08 12:37:54 +0530

    changes made

---
+++ to-do-list-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDoList from './components/toDoList';
+function App() {
+  return (
+   <ToDoList/>
+  );
+}
+
+export default App;
---
+++ to-do-list-app/src/components/toDoList.js
@@ -0,0 +1,16 @@
+import React, {useReducer} from 'react'
+const initialStae = []
+
+function reducer(state, action ){
+    
+}
+function ToDoList() {
+  return (
+    <div>
+        <input>
+        </input>
+    </div>
+  )
+}
+
+export default ToDoList         
